b4f5d4bd3881e2ca86884f40ed50c6cabc13d13b
suhunlin <lin.suhun@msa.hinet.net>
2023-06-07 17:09:16 +0800

    Add layout

--- app/src/main/java/com/suhun/serviceseekbar/MainActivity.java
+++ app/src/main/java/com/suhun/serviceseekbar/MainActivity.java
@@ -3,12 +3,28 @@ package com.suhun.serviceseekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView counterShow;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seek);
+        counterShow = findViewById(R.id.showCounter);
+    }
+
+    public void startServiceFun(View view){
+
+    }
+    public void stopServiceFun(View view){
+
     }
 }
