7ee98a91545fec286c550dc32441a01f209ec34c
suhunlin <lin.suhun@msa.hinet.net>
2023-06-07 17:53:05 +0800

    Add timer and counter to service

--- app/src/main/java/com/suhun/serviceseekbar/MainActivity.java
+++ app/src/main/java/com/suhun/serviceseekbar/MainActivity.java
@@ -2,6 +2,10 @@ package com.suhun.serviceseekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,13 +22,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         seekBar = findViewById(R.id.seek);
+        MyReceiver myReceiver = new MyReceiver();
+        registerReceiver(myReceiver, new IntentFilter("happy"));
         counterShow = findViewById(R.id.showCounter);
     }
 
     public void startServiceFun(View view){
-
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
     }
     public void stopServiceFun(View view){
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+        seekBar.setProgress(0);
+        counterShow.setText(""+0);
+    }
 
+    private class MyReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int maxCounter = intent.getIntExtra("maxCounter", -1);
+            int counter = intent.getIntExtra("counter", -1);
+            if(maxCounter > 0) seekBar.setMax(maxCounter);
+            if(counter > 0){
+                seekBar.setProgress(counter);
+                counterShow.setText(""+counter);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/suhun/serviceseekbar/MyService.java
@@ -0,0 +1,76 @@
+package com.suhun.serviceseekbar;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+    private String tag = MyService.class.getSimpleName();
+    private Timer timer;
+    private UIHandler uiHandler;
+    private int counter, maxCounter;
+    private class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            if(counter>=maxCounter){
+                cancel();
+            }else{
+                uiHandler.sendEmptyMessage(0);
+            }
+        }
+    }
+    private class UIHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            counter++;
+            Intent intent = new Intent("happy");
+            intent.putExtra("counter", counter);
+            sendBroadcast(intent);
+        }
+    }
+
+    public MyService(){
+        uiHandler = new UIHandler();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        timer = new Timer();
+        counter = 0;maxCounter = 100;
+        Intent intent = new Intent("happy");
+        intent.putExtra("maxCounter", maxCounter);
+        sendBroadcast(intent);
+        timer.schedule(new MyTask(), 100, 100);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(timer!=null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+    }
+}
